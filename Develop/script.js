aafb69592231074bbf0a8ef0c3f01336120b1f79
Alexander Leino <Alexander.Leino@yahoo.com>
2021-08-23 15:04:16 -0400

    colors change appropriately

--- Develop/script.js
+++ Develop/script.js
@@ -1,37 +1,48 @@
 var clock = $('#clock')
-var now
+var currentTime
+var currentDay
 var time9am = moment().hour(9).minute(0).second(0)
 var time10am = moment().hour(10).minute(0).second(0)
 var time11am = moment().hour(11).minute(0).second(0)
 var time12pm = moment().hour(12).minute(0).second(0)
 var time1pm = moment().hour(13).minute(0).second(0)
-var time2pm = moment().hour(14).minute(0).second(0)
-var time3pm = moment().hour(15).minute(0).second(0)
-var time4pm = moment().hour(16).minute(0).second(0)
-var time5pm = moment().hour(17).minute(0).second(0)
-console.log(`The current hour is 9am ${time9am.hour()}`)
-console.log(`The current hour is 5pm ${time5pm.hour()}`)
+var time1pm = moment().hour(14).minute(0).second(0)
+var time2pm = moment().hour(15).minute(0).second(0)
+var time3pm = moment().hour(16).minute(0).second(0)
+var time4pm = moment().hour(17).minute(0).second(0)
+var time5pm = moment().hour(18).minute(0).second(0)
 
-function test () {
-    if(time9am.hour() < time5pm.hour()) {
-        console.log('The test worked')
+function updatePlanner() {
+    if(currentTime > time9am.hour()){
+        console.log('it works')
+        $('#time-9am').toggleClass('past')
     }
-    else{
-        console.log('The Test Failed')
+    else if(currentTime = time9am.hour()){
+        $('#time-9am').toggleClass('present')
+    }
+    else {
+        $('time-9am').toggleClass('future')
+    }
+
+    if(currentTime > time5pm.hour()){
+        console.log('it works')
+        $('#time-5pm').toggleClass('past')
+    }
+    else if(currentTime == time5pm.hour()){
+        $('#time-5pm').toggleClass('present')
+    }
+    else {
+        console.log('We should be logging this')
+        $('#time-5pm').toggleClass('future')
     }
 }
-function checkTime () {
-    if(now.hour() > time9am.hour()){
-        
-    }
-}
-
+    
 function updateClock(){
-    now = moment().format("dddd, MMM, Do")
-    clock.text(now)
-    console.log(now)}
+    currentDay = moment().format("dddd, MMM, Do")
+    currentTime = moment().hour()
+    }
 
 setInterval(updateClock, 1000)
-updateClock(
-)
-test()
+
+updateClock()
+updatePlanner()
