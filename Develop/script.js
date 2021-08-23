b1f05bbd9cb96c06f28c97be62f781424c6b2642
Alexander Leino <Alexander.Leino@yahoo.com>
2021-08-23 18:20:45 -0400

    times are being coded correctly

--- Develop/script.js
+++ Develop/script.js
@@ -6,11 +6,11 @@ var time10am = moment().hour(10).minute(0).second(0)
 var time11am = moment().hour(11).minute(0).second(0)
 var time12pm = moment().hour(12).minute(0).second(0)
 var time1pm = moment().hour(13).minute(0).second(0)
-var time1pm = moment().hour(14).minute(0).second(0)
-var time2pm = moment().hour(15).minute(0).second(0)
-var time3pm = moment().hour(16).minute(0).second(0)
-var time4pm = moment().hour(17).minute(0).second(0)
-var time5pm = moment().hour(18).minute(0).second(0)
+var time2pm = moment().hour(14).minute(0).second(0)
+var time3pm = moment().hour(15).minute(0).second(0)
+var time4pm = moment().hour(16).minute(0).second(0)
+var time5pm = moment().hour(17).minute(0).second(0)
+
 
 function updatePlanner() {
     if(currentTime > time9am.hour()){
@@ -23,6 +23,77 @@ function updatePlanner() {
     else {
         $('time-9am').toggleClass('future')
     }
+
+    if(currentTime > time10am.hour()){
+        $('#time-10am').toggleClass('past')
+    }
+    else if(currentTime = time9am.hour()){
+        $('#time-10am').toggleClass('present')
+    }
+    else {
+        $('time-10am').toggleClass('future')
+    }
+
+    if(currentTime > time11am.hour()){
+        $('#time-11am').toggleClass('past')
+    }
+    else if(currentTime = time11am.hour()){
+        $('#time-11am').toggleClass('present')
+    }
+    else {
+        $('time-11am').toggleClass('future')
+    }
+
+    if(currentTime > time12pm.hour()){
+        $('#time-12pm').toggleClass('past')
+    }
+    else if(currentTime = time12pm.hour()){
+        $('#time-12am').toggleClass('present')
+    }
+    else {
+        $('time-12pm').toggleClass('future')
+    }
+    
+    if(currentTime > time1pm.hour()){
+        $('#time-1pm').toggleClass('past')
+    }
+    else if(currentTime = time1pm.hour()){
+        $('#time-1pm').toggleClass('present')
+    }
+    else {
+        $('time-1pm').toggleClass('future')
+    }
+
+    if(currentTime > time2pm.hour()){
+        $('#time-2pm').toggleClass('past')
+    }
+    else if(currentTime = time1pm.hour()){
+        $('#time-2pm').toggleClass('present')
+    }
+    else {
+        $('time-2pm').toggleClass('future')
+    }
+    
+    if(currentTime > time3pm.hour()){
+        $('#time-3pm').toggleClass('past')
+    }
+    else if(currentTime = time1pm.hour()){
+        $('#time-3pm').toggleClass('present')
+    }
+    else {
+        $('time-3pm').toggleClass('future')
+    }
+
+    if(currentTime > time4pm.hour()){
+        $('#time-4pm').toggleClass('past')
+    }
+    else if(currentTime = time4pm.hour()){
+        $('#time-4pm').toggleClass('present')
+    }
+    else {
+        $('time-4pm').toggleClass('future')
+    }
+
 
     if(currentTime > time5pm.hour()){
         console.log('it works')
@@ -40,9 +111,13 @@ function updatePlanner() {
 function updateClock(){
     currentDay = moment().format("dddd, MMM, Do")
     currentTime = moment().hour()
+    clock.text(currentDay)
     }
 
 setInterval(updateClock, 1000)
 
+function retrieveList(){
+    
+}
 updateClock()
 updatePlanner()
